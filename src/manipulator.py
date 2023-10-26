03c845202818747cb4f2a1db6aac96a757aea200
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-10-26 16:12:19 -0500

    stub manipulator

---
+++ src/manipulator.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+"""
+Combines column1 and column2 in the data frame, placing the results into outputColumn
+
+Returns nothing
+"""
+def merge_columns(data: pd.DataFrame, column1: str, column2: str, outputColumn: str):
+    raise "TODO"
