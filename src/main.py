db2c5d470cf3fd991d0facd94eca55a89127bb94
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-10-26 13:04:54 -0500

    add data loading

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from sys import argv, exit
+from os.path import isfile
+import pandas as pd
+
+path = " ".join(argv[:1])
+if not isfile(path):
+    print("Please provide the path to the data!")
+    exit(1)
+
+print("Loading data")
+try:
+    data = pd.read_csv(path)
+except Exception as e:
+    print("Failed to load data:", e)
+    exit(2)
+
