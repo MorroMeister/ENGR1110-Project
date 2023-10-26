da8e94f7ad3a2cc2b9934cf5e4f438d9ae220234
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-10-26 13:06:05 -0500

    flip argv slice around

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from sys import argv, exit
 from os.path import isfile
 import pandas as pd
 
-path = " ".join(argv[:1])
+path = " ".join(argv[1:])
 if not isfile(path):
     print("Please provide the path to the data!")
     exit(1)
