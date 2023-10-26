035aa74368132c5fd91db942716a72d6e5e8a920
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-10-26 15:43:09 -0500

    stub plotter

---
+++ src/plotter.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
+"""
+Plots the given columns from the dataframe and saves it to the specified file as a PNG
+
+Returns true on success, false on failure
+"""
+def plot(data: DataFrame, columns: list[str], file_name: str) -> bool:
+    raise "TODO"
