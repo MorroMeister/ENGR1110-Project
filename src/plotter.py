7adcfc6ae37bdb6e31a64fa214df9881eddf262c
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-11-02 16:37:18 -0500

    Update plotter.py

--- src/plotter.py
+++ src/plotter.py
@@ -8,17 +8,16 @@ Plots the given columns from the dataframe and saves it to the specified file as
 Returns true on success, false on failure
 """
 def plot(data: DataFrame, columns: list[str], file_name: str) -> bool:
-    
-    #creates a figure and axis
+    # Creates a figure and axis
     fig, ax = plt.subplots(layout='constrained')
 
+    # Setup axis formatting
     ax.xaxis.set_major_locator(ticker.MultipleLocator(12*5))
     ax.tick_params(labelrotation=90)
 
-    for x in columns:
-        
-        ax.plot(data['date'], data[x])
+    # Plot columns
+    for y in columns:
+        ax.plot(data['date'], data[y])
 
-
-
+    # Save as image
     fig.savefig(file_name + ".png")
