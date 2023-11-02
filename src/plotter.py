160d0148fd6d6ff3b589727dc32363fd79d79cf1
MorroMeister <148912487+MorroMeister@users.noreply.github.com>
2023-11-02 16:35:54 -0500

    Update plotter.py

--- src/plotter.py
+++ src/plotter.py
@@ -1,3 +1,4 @@
+from matplotlib import ticker
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 
@@ -7,4 +8,17 @@ Plots the given columns from the dataframe and saves it to the specified file as
 Returns true on success, false on failure
 """
 def plot(data: DataFrame, columns: list[str], file_name: str) -> bool:
-    raise "TODO"
+    
+    #creates a figure and axis
+    fig, ax = plt.subplots(layout='constrained')
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(12*5))
+    ax.tick_params(labelrotation=90)
+
+    for x in columns:
+        
+        ax.plot(data['date'], data[x])
+
+
+
+    fig.savefig(file_name + ".png")
