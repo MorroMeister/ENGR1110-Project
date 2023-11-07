f9965ab206c2b4b674ef9d887209f01a293b5fd0
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-11-07 17:15:25 -0600

    impl show_image

--- src/gui.py
+++ src/gui.py
@@ -19,9 +19,39 @@ Sets the size of the window and exits
 """
 def set_size_and_center(w: int, h: int) -> None:
     set_window_size(w, h)
-    sw = get_screen_width()
-    sh = get_screen_height()
+    m = get_current_monitor()
+    sw = get_monitor_width(m)
+    sh = get_monitor_height(m)
     set_window_position((sw - w) // 2, (sh - h) // 2)
+
+"""
+Draws a button
+Returns if the button has been clicked
+"""
+def draw_button(x: int, y: int, text: str, width: int | None = None, highlight: bool = False) -> bool:
+    text_w = measure_text(text, FONT_SIZE)
+    if width == None:
+        width = text_w + (2 * PADDING)
+    
+    # Outline
+    draw_rectangle_lines(x, y, width, BUTTON_HEIGHT, BLACK)
+
+    # Hover + click handler
+    clicked = False
+    mx = get_mouse_x() - x
+    my = get_mouse_y() - y
+    if mx >= 0 and mx <= width and my >= 0 and my <= BUTTON_HEIGHT:
+        draw_rectangle(x + 1, y + 1, width - 2, BUTTON_HEIGHT - 2, GRAY)
+        clicked = is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
+    # Highlight
+    elif highlight:
+        draw_rectangle(x + 1, y + 1, width - 2, BUTTON_HEIGHT - 2, GREEN)
+
+    # Text
+    text_x = (width - text_w) // 2
+    draw_text(text, x + text_x, y + PADDING, FONT_SIZE, BLACK)
+
+    return clicked
 
 """
 Opens a menu where the user can choose columns to plot
@@ -33,8 +63,19 @@ def choose_columns() -> [str]:
 """
 Shows an image to the user
 """
-def show_image(file: str):
-    raise "TODO"
+def show_image(file: str) -> None:
+    img = load_texture(file)
+    set_size_and_center(img.width + (2 * PADDING), PADDING + img.height + PADDING + BUTTON_HEIGHT + PADDING)
+    back_pressed = False
+    while not window_should_close() and not back_pressed:
+        begin_drawing()
+        clear_background(WHITE)
+        draw_texture(img, PADDING, PADDING, WHITE)
+        if draw_button(PADDING, PADDING + img.height + PADDING, "Back", width=img.width):
+            back_pressed = True
+            clear_background(WHITE)
+        end_drawing()
+    unload_texture(img)
 
 """
 The entrypoint for the GUI
@@ -44,7 +85,7 @@ def open_gui(data: DataFrame) -> None:
     init_window(100, 100, TITLE)
     set_exit_key(0)
     while not window_should_close():
-        cols = choose_columns()
+        cols = ["unrate_men", "unrate_women"]#choose_columns()
         # If invalid choice then retry
         if not cols or len(cols) == 0:
             continue
@@ -52,7 +93,7 @@ def open_gui(data: DataFrame) -> None:
         FILE_NAME = "temp_file"
         if not plot(data, cols, FILE_NAME):
             raise "Failed to plot"
-        
+        FILE_NAME += ".png"
         show_image(FILE_NAME)
 
         remove(FILE_NAME)
