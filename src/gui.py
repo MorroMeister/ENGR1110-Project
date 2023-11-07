017d0c57c8dfbc6aed738f6b9dd81b055c52e4e8
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-11-07 16:50:11 -0600

    add cosntants

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,22 @@
 from pandas import DataFrame
 from pyray import *
+import plotter
+
+TITLE = "Group 3 Project"
+FONT_SIZE = 16
+PADDING = 4
+COLUMNS = [
+    ("Total % Unemployment", "unrate"),
+    ("Male % Unemployment", "unrate_men"),
+    ("Female % Unemployment", "unrate_women"),
+    # TODO: more columns here
+]
+
+def set_size_and_center(w: int, h: int):
+    raise "TODO"
+
+def choose_columns() -> [str]:
+    raise "TODO"
 
 def open_gui(data: DataFrame):
     raise "TODO"
